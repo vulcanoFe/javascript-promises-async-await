19f9af33f6727cab0365f68b65ab769d8fd88a6a
Federico <vulcanofederico94@gmail.com>
2023-02-07 17:15:19 +0100

    task2

--- src/index.js
+++ src/index.js
@@ -1,2 +1,14 @@
+import { fetchWithTimeout } from "./services";
+let movies = require("./data/movies.json");
+
 export function fetchMovies() {
+  const resolveFunction = () => movies;
+  fetchWithTimeout(1000).then(resolveFunction);
 }
+
+let moviePromise = fetchMovies();
+moviePromise.then(
+  (results) => {
+    console.log(results);
+  }
+)
