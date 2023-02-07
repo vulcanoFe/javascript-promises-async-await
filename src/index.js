96af4083a50556a86fca7671eff15bdbbee8fda4
Federico <vulcanofederico94@gmail.com>
2023-02-07 17:36:08 +0100

    task3

--- src/index.js
+++ src/index.js
@@ -12,11 +12,11 @@ function getBooksAndMovies() {
 const getBooksAndMoviesPromise = getBooksAndMovies();
 getBooksAndMoviesPromise.then(results => console.log("getBooksAndMoviesPromise", results));
 
-function getBookOrMovies() {
+function getBooksOrMovies() {
   return Promise.race([fetchBooks(), fetchMovies()])
     .then(results => results)
     .catch(error => console.log("Error waiting for the promise race", error));
 }
 
-const getBookOrMoviesPromise = getBookOrMovies();
+const getBookOrMoviesPromise = getBooksOrMovies();
 getBookOrMoviesPromise.then(results => console.log("getBookOrMoviesPromise", results));
