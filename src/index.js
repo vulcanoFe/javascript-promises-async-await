22c88409ebc49ad4900487d9acbcf213d883a298
Federico <vulcanofederico94@gmail.com>
2023-02-08 12:57:28 +0100

    task7

--- src/index.js
+++ src/index.js
@@ -38,12 +38,8 @@ async function getBooksAndMoviesAsync() {
 }
 
 async function getBooksOrMoviesAsync() {
-  try {
-    const values = await Promise.race([asyncFetchBooks(), asyncFetchMovies()]);
-    return values;
-  } catch (error) {
-    console.error("Error waiting for the promise race", error);
-  }
+  const values = await Promise.race([asyncFetchBookss(), asyncFetchMovies()]);
+  return values;
 }
 
 getBooksAndMoviesAsync()
