03524e617e65024a18ea64aa9e63c9085cf40ea9
Federico <vulcanofederico94@gmail.com>
2023-02-08 14:06:12 +0100

    task7

--- src/index.js
+++ src/index.js
@@ -50,7 +50,7 @@ async function getBooksAndMoviesAsync() {
 }
 
 async function getBooksOrMoviesAsync() {
-  const values = await Promise.race([asyncFetchBooks(), asyncFetchMovies()]);
+  const values = await Promise.race([asyncFetchBookss(), asyncFetchMovies()]);
   return values;
 }
 
