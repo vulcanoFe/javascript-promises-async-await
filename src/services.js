34ddf04aa9415872dcd102143e392d734c6c6c21
Federico <vulcanofederico94@gmail.com>
2023-02-08 12:41:12 +0100

    task5

--- src/services.js
+++ src/services.js
@@ -20,7 +20,17 @@ export async function asyncFetchMovies() {
   try {
     const response = await fetch("./data/movies.json");
     const results = await response.json();
-    return results
+    return results;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function asyncFetchBooks() {
+  try {
+    const response = await fetch("./data/books.json");
+    const results = await response.json();
+    return results;
   } catch (error) {
     console.log(error);
   }
